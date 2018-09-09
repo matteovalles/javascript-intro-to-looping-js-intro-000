1fb0ec97f1ea7b21fc523e8117881b4bfa771ba9
Matteo Valles <matteovalles@gmail.com>
2018-09-09 17:45:26 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,11 @@
 function forLoop (array) {
-  for (let i = 0; i < 25; i++) {
+  for (let i = 1; i <= 25; i++) {
     var string = ''
     if(i===1) {
       string = `I am 1 strange loop.`
     } else {
       string = `I am ${i} strange loops.`
     }
-    array(i) = string
+    array(i-1) = string
   }
 }
